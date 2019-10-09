99314c596f5b0f3aef3df1dc9b06218ad4e65cae
beamerhvh <56327041+beamerhvh@users.noreply.github.com>
2019-10-08 18:48:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 // Configuration
 const prefix = "$";
-const owner_ids = ["492975557445943296"];
+const owner_ids = ["374757381093195776"];
 
 function isOwner(id) {
     for(var i of owner_ids) {
